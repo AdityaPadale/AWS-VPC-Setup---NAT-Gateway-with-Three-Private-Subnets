da773dca39d2d75c5980f4873c645e4edc2d9823
Aditya Padale <160272027+AdityaPadale@users.noreply.github.com>
2025-12-12 19:35:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ resource "aws_eip" "nat_eip" {
 }
 
 resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nnat_eip.id
+  allocation_id = aws_eip.nat_eip.id
   subnet_id     = aws_subnet.public.id
 
   tags = { Name = "${var.project_name}-natgw" }
